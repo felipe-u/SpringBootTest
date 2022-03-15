4a5eff67af706bfb95be530a47fb6a9ea1cdb132
felipe-u <felipe_uv@outlook.com>
2022-03-15 00:06:28 -0500

    Initial commit

---
+++ src/main/java/com/peep/core/controllers/ControllerBasic.java
@@ -0,0 +1,16 @@
+package com.peep.core.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class ControllerBasic {
+	
+	@GetMapping(path = {"/post","/"})
+	public String saludar() {
+		return "index";
+	}
+	
+}
